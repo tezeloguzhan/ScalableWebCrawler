4b8cf424ed157998d1a4195dd5893829774a75db
Oguzhan Tezel <tezeloguzhann@gmail.com>
2023-04-17 05:49:22 +0300

    config file env added for CRAWLER_URL

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-CRAWLER_URL="https://www.trendyol.com"
+CRAWLER_URL=os.getenv('CRAWLER_URL', 'http://example.com')
 REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
 REDIS_PORT = os.getenv('REDIS_PORT', 6379)
-REDIS_DB = os.getenv('REDIS_DB', 1)
+REDIS_DB = os.getenv('REDIS_DB', 0)
 
 MAX_CONCURRENT_REQUESTS = 100
 REQUEST_TIMEOUT = 10
